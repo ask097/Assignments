b0216b5c24e853c518fd34f0a7dd0f2e7ae29e27
Skundberg Andreas Reiten (ask097) <ask097@rstudio.uit.no>
2020-09-30 13:39:05 +0200

    Assignment 3: resubmit

--- Assignment 3 - web scraping DataCamp.R
+++ Assignment 3 - web scraping DataCamp.R
@@ -7,8 +7,6 @@ library(rvest)
 
 datacamp_r <- read_html("https://www.datacamp.com/courses/tech:r")
 
-# Scraping fist header by scraping title within html node
-
 
 # Scraping titles of all R courses by selecting "h4" within html nodes
 datacamp_r <-datacamp_r %>% 
@@ -25,12 +23,12 @@ datacamp_python <-datacamp_python %>%
   html_nodes("h4") %>%
   html_text()
 
-# Use cbind to 
+# Use cbind to bind columns
 
 datacamp_r <- cbind (datacamp_r)
 datacamp_python <- cbind(datacamp_python)
 
-
+# Then assign title to the dataframe
 
 Language <- ("R")
 datacamp_r <- cbind(datacamp_r,Language)
@@ -38,9 +36,12 @@ datacamp_r <- cbind(datacamp_r,Language)
 Language2 <- ("Python")
 datacamp_python <- cbind(datacamp_python,Language2)
 
+# Naming the colums
 
 colnames(datacamp_r) <- c ("Tech", "Language")
 colnames(datacamp_python) <- c ("Tech", "Language")
+
+# Combinging the two dataframes into a large dataframe listing each course with its program
 
 largedf <- rbind(datacamp_r,datacamp_python)
 
